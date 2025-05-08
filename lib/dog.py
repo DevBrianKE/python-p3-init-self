dc69a4995cd839cbc370e30466d6083945b7ec20
DevBrianKE <kipchumbabrian47@gmail.com>
2025-05-08 20:49:05 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name,breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+fido = Dog("Fido")
+#     def bark(self):
+#         print("Woof!")
+
+#     def showing_self(self):
+#         return self
+
+# fido = Dog("Fido")
+# fido.name
+
+# # fido.breed = "Dalmatian"
+
+# # snoopy = Dog()
+# # snoopy.breed = "Beagle"
+
+# # print(fido.breed)
+# # print(snoopy.breed)
